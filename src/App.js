8c27c222d50d06d3a501cd782b7fa8f038783a0d
Diene Ferreira <87545136+Diene89@users.noreply.github.com>
2022-03-10 17:36:59 -0300

    Aplicando sugestões do Code Review.
    Co-authored-by: trkotovicz <93948349+trkotovicz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import { Home } from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
         </Switch>
-      </BrowserRouter>
     </div>
   );
 }

1bde622c3599a01db21daffae6ee2714e45a059c
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-10 18:48:47 -0300

    Requisito 3 - Completo (code-review)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Home } from './pages/Home';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route path="/shopping-cart" component={ Cart } />
           <Route exact path="/" component={ Home } />
         </Switch>
       </BrowserRouter>

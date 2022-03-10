eb0cb64ea826d479af15f1d51d0c794abecd82b4
Diene Ferreira <87545136+Diene89@users.noreply.github.com>
2022-03-10 17:29:51 -0300

    Aplicando sugestões do Code Review.
    Co-authored-by: trkotovicz <93948349+trkotovicz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
     </div>
